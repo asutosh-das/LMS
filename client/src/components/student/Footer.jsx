a8c937d9b278cc343f8b72e0b85cbb686dca8e18
asutosh-das <ashutoshdas788@gmail.com>
2026-02-23 19:53:43 +0530

    Footer part-1 embedded

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    <footer className="bg-gray-900 md:px-36 text-left w-full mt-10">
+      <div className="flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30">
+        <div className="flex flex-col md:items-start items-center w-full">
+          <img src={assets.logo_dark} alt="logo" />
+          <p className="mt-6 text-center md:text-left text-sm text-white/80">
+            Empowering learners worldwide with practical, accessible courses,
+            personalized support, and real-world projects. Join our community to
+            build skills, advance careers, and unlock lifelong opportunities
+            through hands-on learning and mentorship programs.
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">
+        Copyright 2026 &copy; Edemy. All Right Reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
