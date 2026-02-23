1b7750261e3a6175d7ddbed609affb819e2ca201
asutosh-das <ashutoshdas788@gmail.com>
2026-02-23 20:27:05 +0530

    footer part-3 embedded

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -17,13 +17,32 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <h2 className="font-semibold text-white mb-5">Company</h2>
           <ul className="flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">Privacy policy</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+            <li>
+              <a href="#">Privacy policy</a>
+            </li>
           </ul>
         </div>
-        <div></div>
+        <div className="hidden md:flex flex-col items-start w-full">
+          <h2 className="font-semibold text-white mb-5">
+            Subscribe to our newsletter
+          </h2>
+          <p className="text-sm text-white/80">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+          <div className="flex items-center gap-2 pt-4">
+            <input type="email" placeholder="Enter your email" className="border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm"/>
+            <button className="px-5 py-2 bg-blue-600 hover:bg-blue-700 transition rounded-r-md text-sm font-medium text-white">Subscribe</button>
+          </div>
+        </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm text-white/60">
         Copyright 2026 &copy; Edemy. All Right Reserved.
