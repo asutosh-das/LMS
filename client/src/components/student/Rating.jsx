5fa8da78ab3dd1086f4e440812d37c65e4cbbb45
asutosh-das <ashutoshdas788@gmail.com>
2026-02-26 00:28:21 +0530

    rating component created to handle the rating in the player page

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Rating = () => {
+
+  const [rating,setRating] = useState(initialRating || 0)
+
   return (
     <div>
-      <h1>Rating</h1>
+      {Array.from({length: 5}, (_, index)=>{
+        const starValue = index + 1;
+        return (
+          <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400' }`}>
+            &#9733;
+          </span>
+        )
+      })}
     </div>
   )
 }
