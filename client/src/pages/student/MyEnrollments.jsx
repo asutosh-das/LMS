5be57c8fe063edf5348a4ef12959d3e416b3f8cd
asutosh-das <ashutoshdas788@gmail.com>
2026-02-25 20:56:49 +0530

    body part of the table added with static values

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const MyEnrollments = () => {
+
+  const {enrolledCourses, calculateCourseDuration} = useContext(AppContext)
   return (
     <>
     <div className='md:px-36 px-8 pt-10'>
@@ -14,6 +17,29 @@ const MyEnrollments = () => {
             <th className='px-4 py-3 font-semibold truncate'>Status</th>
           </tr>
         </thead>
+        <tbody className='text-gray-700'>
+          {
+            enrolledCourses.map((course,index)=>(
+              <tr key={index} className='border-b border-gray-500/20'>
+                <td className='px-4 py-3 md:px-4 flex items-center space-x-3'>
+                  <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28'/>
+                  <div className='flex-1'>
+                    <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
+                  </div>
+                  </td>
+                  <td className='px-4 py-3 max-sm:hidden'>
+                    {calculateCourseDuration(course)}
+                  </td>
+                  <td className='px-4 py-3 max-sm:hidden'>
+                    4 / 10 <span>Lectures</span>
+                  </td>
+                  <td className='px-4 py-3 max-sm:text-right'>
+                    <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>On Going</button>
+                  </td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
     </>
