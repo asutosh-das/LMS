3d8417973d1301d80c46872444d9ff7ee5249c39
asutosh-das <ashutoshdas788@gmail.com>
2026-03-09 23:54:29 +0530

    Add context and state management to MyCourses component

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../../context/AppContext';
 
 const MyCourses = () => {
+  const {currency, allCOurses} = useContext(AppContext);
+  const [courses, setCourses] = useState(null)
   return (
     <div>
       <h1>My course</h1>
