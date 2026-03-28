57885dbcaf13c4d0f34d7eb61e63539688f2ab93
asutosh-das <ashutoshdas788@gmail.com>
2026-03-29 00:59:00 +0530

    eduactorController add new course api setup

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,25 +1,50 @@
-import { clerkClient, getAuth } from '@clerk/express';
+import { clerkClient, getAuth } from "@clerk/express";
+import { v2 as cloudinary } from "cloudinary";
 
 //update role to educator
 export const updateRoleToEducator = async (req, res) => {
   try {
     const { userId } = getAuth(req);
 
-    
-
     if (!userId) {
       return res.json({
         success: false,
-        message: 'Unauthorized: A valid User ID is required.',
+        message: "Unauthorized: A valid User ID is required.",
       });
     }
 
     await clerkClient.users.updateUserMetadata(userId, {
       publicMetadata: {
-        role: 'educator',
+        role: "educator",
       },
     });
-    res.json({ success: true, message: 'You can publish a course now' });
+    res.json({ success: true, message: "You can publish a course now" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Add new Course
+export const addCourse = async (req, res) => {
+  try {
+    const { courseData } = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
+
+    if (!imageFile) {
+      return res.json({ success: false, message: "Thumbnail not attached" });
+    }
+
+    const parsedCourseData = await JSON.parse(courseData);
+    parsedCourseData.educator = educatorId;
+
+    const newCourse = await Course.create(parsedCourseData);
+
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+    newCourse.courseThumbnail = imageUpload.secure_url;
+    await newCourse.save();
+
+    res.json({ success: true, message: "Course Added" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
