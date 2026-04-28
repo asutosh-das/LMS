0b3370aa8a3f10cd271ff48ca593cf48f70fc23b
asutosh-das <ashutoshdas788@gmail.com>
2026-04-28 21:21:36 +0530

    Add getEducatorCourses API and update educator routes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -20,7 +20,7 @@ export const clerkWebHooks = async (req, res) => {
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.second_name,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
         await User.create(userData);
@@ -30,8 +30,8 @@ export const clerkWebHooks = async (req, res) => {
 
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
-          name: data.first_name + " " + data.second_name,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { requireAuth } from "@clerk/express";
 import {
   addCourse,
+  getEducatorCourses,
   updateRoleToEducator,
 } from "../controllers/educatorController.js";
 import upload from "../configs/multer.js";
@@ -17,5 +18,6 @@ educatorRouter.post(
   protectEducator,
   addCourse,
 );
+educatorRouter.get("/courses", protectEducator, getEducatorCourses);
 
 export default educatorRouter;
